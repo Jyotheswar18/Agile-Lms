802b3ee51cb4d032a55f2ee653b7d43346b6eb5c
Jyotheswar18 <Chinnapapireddygari2023@vitstudent.ac.in>
2026-01-07 22:51:42 +0530

    Added Login feature

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 def reg(name):
   print(f"{name} has been registered.")
+
+#Login Feature code
+def login(user, password):
+    # simple hardcoded credentials
+    correct_user = "admin"
+    correct_password = "1234"
+
+    if user == correct_user and password == correct_password:
+        print("Login successful")
+    else:
+        print("Login failed")
+
+reg("admin")
+login("admin", "1234")
